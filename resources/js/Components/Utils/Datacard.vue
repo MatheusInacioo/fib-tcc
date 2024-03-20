ecaa97cb3015ca704a90a36351983fe0383eb20f
matheus <mtsi1@hotmail.com>
2024-03-20 20:33:07 -0300

    datacard ajustes

---
+++ resources/js/Components/Utils/Datacard.vue
@@ -0,0 +1,19 @@
+<template>
+    <div :class="`datacard-holder flex max-h-full min-w-[420px] border border-l-8 border-l-${settings.color}-500 shadow-lg mr-5 p-2`">
+        <div class="flex flex-col w-full "></div>
+        <div class="w-1/4 flex items-center justify-center">
+            <i class="bx bxs-error text-8xl text-yellow-500"></i>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        settings: {
+            type: Array,
+            default: () => []
+        },
+    },
+}
+</script>
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,28 +1,48 @@
 <template>
     <BaseLayout>
-        <p class="font-medium 2xl:text-3xl text-xl mr-4">Início</p>
-
-        <div class="h-px bg-orange-500 my-4"></div>
-
-        <div class="flex h-full overflow-x-auto">
-            <Databoard v-for="board in boards"/>
+        <p class="font-medium 2xl:text-3xl text-xl mr-4 mb-4">Dashboard</p>
+        <div class="flex h-full overflow-y-auto">
+            <div class="flex h-1/3">
+                <Datacard
+                    v-for="card in cardsSettigs"
+                    :key="card.id"
+                    :settings="cardsSettigs"
+                />
+            </div>
         </div>
     </BaseLayout>
 </template>
 
 <script>
     import BaseLayout from '@/Components/Layout/BaseLayout.vue';
-    import Databoard from '@/Components/Utils/Databoard.vue';
+    import Datacard from '@/Components/Utils/Datacard.vue';
 
     export default {
         components: {
             BaseLayout,
-            Databoard,
+            Datacard,
+        },
+
+        props: {
+
         },
 
         data() {
             return {
-                boards: [0, 1, 2, 3, 4, 5],
+                cardsSettings: [
+                    {
+                        color: 'yellow',
+                        icon: 'bx-error',
+                    },
+                    {
+                        color: 'blue',
+                        icon: 'bx-package',
+                    },
+                    {
+                        color: 'green',
+                        icon: 'bx-dollar',
+                    },
+                ],
             }
         },
     }
