10b3022654145732a00e6ab75e9ad3002ede8c4a
Matheus Inacio <mtsi1@hotmail.com>
2024-04-30 17:50:28 -0300

    Adição de graficos na tela inicial

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -2,28 +2,108 @@
     <BaseLayout>
         <p class="font-medium 2xl:text-2xl text-xl mr-4 mb-4">Dashboard</p>
         <div class="flex h-full overflow-y-auto">
-            <div class="flex h-1/3">
-                <Datacard
-                    v-for="index in 3"
-                    :key="index"
-                />
+            <div class="flex w-full h-full">
+                <div class="flex flex-col mr-4 w-1/2 h-full">
+                    <div class="h-1/2 w-full rounded-lg border border-gray-200 mb-4 p-4">
+                        <Bar
+                            aria-label="Entradas"
+                            id="entryBarChart"
+                            :options="entryChartOptions"
+                            :data="entryChartData"
+                        />
+                    </div>
+                    <div class="h-1/2 w-full rounded-lg border border-gray-200 p-4">
+                        <Bar
+                            aria-label="Saídas"
+                            id="exitBarChart"
+                            :options="exitChartOptions"
+                            :data="exitChartData"
+                        />
+                    </div>
+                </div>
+                <div class="flex rounded-lg shadow-lg w-1/2 h-full border border-gray-200">
+                    
+                </div>
             </div>
         </div>
     </BaseLayout>
 </template>
 
 <script>
-    import BaseLayout from '@/Components/Layout/BaseLayout.vue';
-    import Datacard from '@/Components/Utils/Datacard.vue';
+    import BaseLayout from '@/Components/Layout/BaseLayout.vue'; 
+    import { Bar } from 'vue-chartjs';
+    import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js';
+
+    ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale);
 
     export default {
         components: {
             BaseLayout,
-            Datacard,
+            Bar,
         },
 
         props: {
 
         },
+
+        data() {
+            return {
+                entryChartData: {
+                    labels: [ 
+                        'January', 
+                        'February', 
+                        'March', 
+                        'April', 
+                        'May',
+                        'June',
+                        'July',
+                        'August',
+                        'September',
+                        'October',
+                        'November',
+                        'December',
+                     ],
+                    datasets: [
+                        {
+                            label: 'Entradas',
+                            backgroundColor: '#5591f2',
+                            data: [5, 20, 15, 35, 10, 100, 45, 93, 66, 31, 51, 44]
+                        }
+                    ]
+                },
+
+                entryChartOptions: {
+                  responsive: true
+                },
+
+                exitChartData: {
+                    labels: [ 
+                        'January', 
+                        'February', 
+                        'March', 
+                        'April', 
+                        'May',
+                        'June',
+                        'July',
+                        'August',
+                        'September',
+                        'October',
+                        'November',
+                        'December',
+                     ],
+                    datasets: [
+                        {
+                            label: 'Saídas',
+                            backgroundColor: '#5591f2',
+                            data: [50, 40, 55, 100, 70, 33, 41, 44, 52, 15, 28, 37]
+                        }
+                    ]
+                },
+
+                exitChartOptions: {
+                  responsive: true
+                },
+            }
+        }
     }
 </script>
--- resources/js/Pages/Suppliers/Index.vue
+++ resources/js/Pages/Suppliers/Index.vue
@@ -116,27 +116,27 @@ export default {
     },
 
     created() {
-        var flash = this.$page.props.flash;
+        // var flash = this.$page.props.flash;
 
-        if(flash) {
-            this.showModal = true;
+        // if(flash) {
+        //     this.showModal = true;
 
-             if(flash['create-success']) {
-                this.message = flash['create-success'];
-             } else if (flash['update-success']) {
-                this.message = flash['update-success'];
-             } else if (flash['destroy-success']) {
-                this.message = flash['destroy-success'];
-             }
+        //      if(flash['create-success']) {
+        //         this.message = flash['create-success'];
+        //      } else if (flash['update-success']) {
+        //         this.message = flash['update-success'];
+        //      } else if (flash['destroy-success']) {
+        //         this.message = flash['destroy-success'];
+        //      }
 
-             if(flash['create-error']) {
-                this.message = flash['create-error'];
-             } else if (flash['update-error']) {
-                this.message = flash['update-error'];
-             } else if (flash['destroy-error']) {
-                this.message = flash['destroy-error'];
-             }
-        }
+        //      if(flash['create-error']) {
+        //         this.message = flash['create-error'];
+        //      } else if (flash['update-error']) {
+        //         this.message = flash['update-error'];
+        //      } else if (flash['destroy-error']) {
+        //         this.message = flash['destroy-error'];
+        //      }
+        // }
     },
 }
 </script>
