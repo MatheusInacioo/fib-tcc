d96137d88b74da2e639aa38958c33fd0380a7a1e
Matheus <mtsi1@hotmail.com>
2024-09-23 17:41:10 -0300

    Added company_id into ShopResource class

--- app/Http/Resources/ShopResource.php
+++ app/Http/Resources/ShopResource.php
@@ -17,6 +17,7 @@ class ShopResource extends JsonResource
         return [
             'id' => $this->id,
             'name' => $this->name,
+            'company_id' => $this->company_id,
             'company_name' => $this->getCompanyName(),
             'location' => $this->location,
         ];
