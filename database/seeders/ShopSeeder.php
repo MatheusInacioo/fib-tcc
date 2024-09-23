7dc0319a52e84c6cde42897a494f53bc2e903c4b
Matheus <mtsi1@hotmail.com>
2024-09-23 15:53:10 -0300

    New seeders and fields

---
+++ database/seeders/ShopSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Shop;
+use Illuminate\Database\Seeder;
+
+class ShopSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        for ($i = 0; $i < 2; $i++) {
+            Shop::create([
+                'name' => fake()->word(),
+                'company_id' => 1,
+                'location' => fake()->address(),
+                'active' => true,
+            ]);
+        }
+    }
+}
