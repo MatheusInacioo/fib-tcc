8ed129358f962d4f26ad417968c006de43fa780f
Matheus <mtsi1@hotmail.com>
2024-10-18 17:37:48 -0300

    Removed global scope from Shop class

--- app/Models/Shop.php
+++ app/Models/Shop.php
@@ -6,7 +6,6 @@ use App\Models\Scopes\SessionScope;
 use Illuminate\Database\Eloquent\Attributes\ScopedBy;
 use Illuminate\Database\Eloquent\Model;
 
-#[ScopedBy([SessionScope::class])]
 class Shop extends Model
 {
     protected $table = 'shops';
