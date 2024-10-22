b0d92487949d0995d199a860ad2cf992839db272
Matheus Inacio <mtsi1@hotmail.com>
2024-10-22 18:56:19 -0300

    Changed company dropdown mobile icon

--- resources/js/Components/Layout/Topbar.vue
+++ resources/js/Components/Layout/Topbar.vue
@@ -60,7 +60,7 @@
                     class="user-dropdown flex h-10 items-center transition-all hover:scale-110 cursor-pointer"
                 >
                     <i class="mobile-std:hidden bx bxs-buildings text-3xl text-primary mr-2"></i>
-                    <i class="web:hidden mobile-lg:hidden bx bxs-store text-3xl text-secondary"></i>
+                    <i class="web:hidden mobile-lg:hidden bx bxs-buildings text-3xl text-secondary"></i>
                     <p 
                         v-if="currentShop != null"
                         class="text-base 2xl:text-lg font-medium mr-2 mobile-std:hidden"
